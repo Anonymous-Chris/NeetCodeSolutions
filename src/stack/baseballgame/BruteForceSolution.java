11975ea93ecbec47f4401edc902d854fe5956681
Chris Lingthep/BOSK DT Team/SKO <og03510@skcorp.com>
2026-02-04 09:03:14 -0600

    baseball game

---
+++ src/stack/baseballgame/BruteForceSolution.java
@@ -0,0 +1,38 @@
+package stack.baseballgame;
+
+import arrays.print.PrintItem;
+
+import java.util.ArrayList;
+
+public class BruteForceSolution {
+    static void main(String[] args) {
+        String[] operations = {"5","2","C","D","+"};
+        PrintItem.print(calPoints(operations));
+        String[] operations1 = {"1","2","+","C","5","D"};
+        PrintItem.print(calPoints(operations1));
+    }
+    public static int calPoints(String[] operations) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(String operation: operations){
+            if(operation.equals("C")){
+                arrayList.remove(arrayList.size() - 1);
+            } else if(operation.equals("D")){
+                int lastScore = arrayList.get(arrayList.size() - 1);
+                arrayList.add(lastScore * 2);
+            } else if(operation.equals("+")){
+                int lastScore = arrayList.get(arrayList.size() - 1);
+                int secondLastScore = arrayList.get(arrayList.size() - 2);
+                arrayList.add(lastScore + secondLastScore);
+            } else {
+                arrayList.add(Integer.parseInt(operation));
+            }
+        }
+        int totalScore = 0;
+        for(int score: arrayList){
+            totalScore += score;
+        }
+        return totalScore;
+    }
+}
+//TC: O(n)
+//SC: O(n)
---
+++ src/stack/baseballgame/StackSolution.java
@@ -0,0 +1,41 @@
+package stack.baseballgame;
+
+import arrays.print.PrintItem;
+
+import java.util.Stack;
+
+public class StackSolution {
+    static void main(String[] args) {
+        String[] operations = {"5","2","C","D","+"};
+        PrintItem.print(calPoints(operations));
+        String[] operations1 = {"1","2","+","C","5","D"};
+        PrintItem.print(calPoints(operations1));
+    }
+    public static int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+        for(String operation: operations){
+            if(operation.equals("C")){
+                stack.pop();
+            } else if(operation.equals("D")){
+                int lastScore = stack.peek();
+                stack.push(lastScore * 2);
+            } else if(operation.equals("+")){
+                //need to pop as cannot access second top element directly
+                int lastScore = stack.pop();
+                int secondLastScore = stack.peek();
+                stack.push(lastScore);
+                stack.push(lastScore + secondLastScore);
+            } else {
+                //as it is string need to parse to integer
+                stack.push(Integer.parseInt(operation));
+            }
+        }
+        int totalScore = 0;
+        for(int score: stack){
+            totalScore += score;
+        }
+        return totalScore;
+    }
+}
+//TC: O(n)
+//SC: O(n)
